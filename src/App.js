157e1d78caa3a01b19a25b094c73d47783b36605
Colin Meinecke <cmeinecke@gmail.com>
2022-06-02 20:57:54 -0500

    Async Partners, Promotions, DisplayList

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 import './App.css';
 
 function App() {
@@ -18,7 +19,8 @@ function App() {
   useEffect(() => {
     dispatch(fetchCampsites());
     dispatch(fetchPartners());
-  }, [dispatch]);
+    dispatch(fetchPromotions());
+}, [dispatch]);
 
   return (
     <div className="App">
